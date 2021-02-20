2685cf291c8cec4b9ab196fc786a5de7fac68d4e
Beatriz Cabrero <b.cabrero23@gmail.com>
2021-02-20 11:21:49 +0100

    menu not working properly

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const hamburger = document.querySelector('#hamburger');
+const nav = document.querySelector('.nav');
+const menu = document.querySelector('.menuList');
 hamburger.addEventListener('click', () => {
   console.log("I got clicked");
-  hamburger.classList.toggle('active'); // (Array.from(hamburger.children)).forEach(element => {
-  //     element.classList.toggle("active");
-  //     console.log(element.classList);
-  // });
+  hamburger.classList.toggle('active');
+  nav.classList.toggle('visible');
+  menu.classList.toggle('visible');
 });
--- ts/index.ts
+++ ts/index.ts
@@ -1,10 +1,10 @@
 'use strict'
 const hamburger = (document.querySelector('#hamburger') as HTMLElement);
+const nav = (document.querySelector('.nav') as HTMLElement);
+const menu = (document.querySelector('.menuList') as HTMLElement);
 hamburger. addEventListener('click', () : void =>{
     console.log("I got clicked");
     hamburger.classList.toggle('active');
-    // (Array.from(hamburger.children)).forEach(element => {
-    //     element.classList.toggle("active");
-    //     console.log(element.classList);
-    // });
+    nav.classList.toggle('visible');
+    menu.classList.toggle('visible');
 });
